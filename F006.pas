ab0bb60cf7af6b992532e1832e457634f5002f9f
mnetoo <163374075+mnetoo@users.noreply.github.com>
2024-07-04 22:01:26 -0300

    F006.pas

---
+++ F006.pas
@@ -0,0 +1,30 @@
+program compara_datas;
+
+var 
+    dia1, mes1, ano1, dia2, mes2, ano2: longint;
+
+function eh_anterior (d1, m1, a1, d2, m2, a2: longint): boolean;
+begin
+    
+    if (a1 > a2) then
+        eh_anterior:= false
+    else if (a1 = a2) and (m1 > m2) then
+        eh_anterior:= false
+    else if (a1 = a2) and (m1 = m2) and (d1 >= d2) then    
+        eh_anterior:= false
+    else
+        eh_anterior:= true;
+
+end;
+
+
+begin
+
+    read (dia1, mes1, ano1, dia2, mes2, ano2);
+
+        if eh_anterior (dia1, mes1, ano1, dia2, mes2, ano2) then
+            writeln ('a primeira data eh anterior')
+        else
+            writeln ('a primeira data nao eh anterior');
+
+end.
